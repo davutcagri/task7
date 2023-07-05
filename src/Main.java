808ac76960b97a55fbdb84e1a895a977419e7279
davutcagri <71286320+davutcagri@users.noreply.github.com>
2023-07-05 14:08:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,71 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //1. ceil
+        System.out.println("Math.ceil(2.4): " + Math.ceil(2.4));
+
+        //2. floor
+        System.out.println("Math.floor(3.6): " + Math.floor(3.6));
+
+        //3. cos
+        System.out.println("Math.cos(60): " + Math.cos(60));
+
+        //4. sin
+        System.out.println("Math.sin(30): " + Math.sin(30));
+
+        //5. tan
+        System.out.println("Math.tan(30): " + Math.tan(30));
+
+        //6. acos
+        System.out.println("Math.acos(30): " + Math.acos(30));
+
+        //7. asin
+        System.out.println("Math.asin(30): " + Math.asin(30));
+
+        //8. max
+        System.out.println("Math.max(1.6, 8): " + Math.max(1.6, 8));
+
+        //9. min
+        System.out.println("Math.min(7, 2.3): " + Math.min(7, 2.3));
+
+        //10. pow
+        System.out.println("Math.pow(4, 2): " + Math.pow(4, 2));
+
+        //11. random
+        System.out.println("Math.random(): " + Math.random());
+
+        //12. round
+        System.out.println("Math.round(2.4): " + Math.round(2.4));
+        System.out.println("Math.round(7.1): " + Math.round(7.1));
+
+        //13. sqrt
+        System.out.println("Math.sqrt(36): " + Math.sqrt(36));
+
+        //14.log
+        System.out.println("Math.log(26): " + Math.log(26));
+
+        //15. log1p
+        System.out.println("Math.log1p(5): " + Math.log1p(5));
+
+        //16. log10
+        System.out.println("Math.log10(10): " + Math.log10(10));
+
+        //17. exp
+        System.out.println("Math.exp(2): " + Math.exp(2));
+
+        //18. expm1
+        System.out.println("Math.expm1(28): " + Math.expm1(28));
+
+        //19. abs
+        System.out.println("Math.abs(120): " + Math.abs(-120.1212));
+
+        //20. cbrt
+        System.out.println("Math.cbrt(81): " + Math.cbrt(81));
+
+        //21. nextAfter
+        System.out.println("Math.nextAfter(84352.24): " + Math.nextAfter(84352.24, 154.284));
+
+        //22. nextUp
+        System.out.println("Math.nextUp(676): " + Math.nextAfter(676, 123));
+    }
+}
